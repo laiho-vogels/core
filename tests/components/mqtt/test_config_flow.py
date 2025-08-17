5b84a2c4608a627e48d6bca89115820fdb075695
jbouwh <jan@jbsoft.nl>
2025-08-17 17:41:41 +0000

    Add retain missed option

--- tests/components/mqtt/test_config_flow.py
+++ tests/components/mqtt/test_config_flow.py
@@ -2686,6 +2686,7 @@ async def test_migrate_of_incompatible_config_entry(
                 "code_arm_required": True,
                 "code_disarm_required": True,
                 "code_trigger_required": True,
+                "retain": False,
                 "alarm_control_panel_payload_settings": {
                     "payload_arm_away": "ARM_AWAY",
                     "payload_arm_custom_bypass": "ARM_CUSTOM_BYPASS",
@@ -2731,6 +2732,7 @@ async def test_migrate_of_incompatible_config_entry(
                 "code_arm_required": True,
                 "code_disarm_required": True,
                 "code_trigger_required": True,
+                "retain": False,
                 "alarm_control_panel_payload_settings": {
                     "payload_arm_away": "ARM_AWAY",
                     "payload_arm_custom_bypass": "ARM_CUSTOM_BYPASS",
@@ -2761,6 +2763,7 @@ async def test_migrate_of_incompatible_config_entry(
                 "code_arm_required": True,
                 "code_disarm_required": True,
                 "code_trigger_required": True,
+                "retain": False,
                 "alarm_control_panel_payload_settings": {
                     "payload_arm_away": "ARM_AWAY",
                     "payload_arm_custom_bypass": "ARM_CUSTOM_BYPASS",
